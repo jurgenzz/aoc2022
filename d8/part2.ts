a3ae3906f2ad16d6e4439293bce297b162dabcf4
jurgenzz <jurgis.kacens@gmail.com>
2022-12-08 11:53:36 +0200

    improve d8/p2

--- d8/part2.ts
+++ d8/part2.ts
@@ -12,24 +12,18 @@ for (let i = 0; i < data.length; i++) {
     const left = trees.slice(0, idx).reverse()
     const right = trees.slice(idx + 1)
 
-    const leftIndex = left.findIndex((v) => v >= tree)
-    const visibleFromLeft = leftIndex === -1 ? left.length : leftIndex + 1
-    const rightIndex = right.findIndex((v) => v >= tree)
-    const visibleFromRight = rightIndex === -1 ? right.length : rightIndex + 1
+    const visibleFromLeft = left.findIndex((v) => v >= tree) + 1 || left.length
+    const visibleFromRight = right.findIndex((v) => v >= tree) + 1 || right.length
 
     // visible from top & bottom
     const columns = data.map((row) => row[idx])
     const top = columns.slice(0, i).reverse()
     const bottom = columns.slice(i + 1)
 
-    const topIndex = top.findIndex((v) => v >= tree)
-    const visibleFromTop = topIndex === -1 ? top.length : topIndex + 1
-    const bottomIndex = bottom.findIndex((v) => v >= tree)
-    const visibleFromBottom =
-      bottomIndex === -1 ? bottom.length : bottomIndex + 1
+    const visibleFromTop = top.findIndex((v) => v >= tree) + 1 || top.length
+    const visibleFromBottom = bottom.findIndex((v) => v >= tree) + 1 || bottom.length
 
-    const sum =
-      visibleFromBottom * visibleFromTop * visibleFromRight * visibleFromLeft
+    const sum = visibleFromBottom * visibleFromTop * visibleFromRight * visibleFromLeft
 
     if (sum > highest) {
       highest = sum
