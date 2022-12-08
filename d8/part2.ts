48980ad5103c50b68eb0057cf22e0cd7aeaa64a3
jurgenzz <jurgis.kacens@gmail.com>
2022-12-08 11:36:41 +0200

    improve d8/p2

--- d8/part2.ts
+++ d8/part2.ts
@@ -12,49 +12,21 @@ for (let i = 0; i < data.length; i++) {
     const left = trees.slice(0, idx).reverse()
     const right = trees.slice(idx + 1)
 
-    let visibleFromLeft = 0
-    for (let i2 = 0; i2 < left.length; i2++) {
-      const t = left[i2]
+    const leftIndex = left.findIndex((v) => v >= tree)
+    const visibleFromLeft = leftIndex === -1 ? left.length : leftIndex + 1
+    const rightIndex = right.findIndex((v) => v >= tree)
+    const visibleFromRight = rightIndex === -1 ? right.length : rightIndex + 1
 
-      visibleFromLeft++
-      if (t >= tree) {
-        break
-      }
-    }
-
-    let visibleFromRight = 0
-    for (let i2 = 0; i2 < right.length; i2++) {
-      const t = right[i2]
-
-      visibleFromRight++
-      if (t >= tree) {
-        break
-      }
-    }
     // visible from top & bottom
     const columns = data.map((row) => row[idx])
     const top = columns.slice(0, i).reverse()
     const bottom = columns.slice(i + 1)
 
-    let visibleFromBottom = 0
-    for (let i2 = 0; i2 < bottom.length; i2++) {
-      const t = bottom[i2]
-
-      visibleFromBottom++
-      if (t >= tree) {
-        break
-      }
-    }
-
-    let visibleFromTop = 0
-    for (let i2 = 0; i2 < top.length; i2++) {
-      const t = top[i2]
-
-      visibleFromTop++
-      if (t >= tree) {
-        break
-      }
-    }
+    const topIndex = top.findIndex((v) => v >= tree)
+    const visibleFromTop = topIndex === -1 ? top.length : topIndex + 1
+    const bottomIndex = bottom.findIndex((v) => v >= tree)
+    const visibleFromBottom =
+      bottomIndex === -1 ? bottom.length : bottomIndex + 1
 
     const sum =
       visibleFromBottom * visibleFromTop * visibleFromRight * visibleFromLeft
@@ -66,3 +38,4 @@ for (let i = 0; i < data.length; i++) {
 }
 
 console.log(highest)
+// 230112
